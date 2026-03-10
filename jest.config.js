09a4dec841366702deb2bd64098b89c7d18386fc
Diego Saá <cuco.saa@gmail.com>
2026-03-10 01:14:54 -0500

    fix: align mobile jest setup with Expo 54

--- jest.config.js
+++ jest.config.js
@@ -4,20 +4,4 @@ module.exports = {
   preset: 'jest-expo',
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   testPathIgnorePatterns: ['/node_modules/', '/e2e/'],
-  transformIgnorePatterns: [
-    'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation|@expo|expo(nent)?|@expo-google-fonts|@unimodules|unimodules|sentry-expo|native-base|react-clone-referenced-element)',
-  ],
-  moduleNameMapper: {
-    '^react$': ['<rootDir>/node_modules/react', '<rootDir>/../node_modules/react', 'react'],
-    '^react/jsx-runtime$': [
-      '<rootDir>/node_modules/react/jsx-runtime',
-      '<rootDir>/../node_modules/react/jsx-runtime',
-      'react/jsx-runtime',
-    ],
-    '^react/jsx-dev-runtime$': [
-      '<rootDir>/node_modules/react/jsx-dev-runtime',
-      '<rootDir>/../node_modules/react/jsx-dev-runtime',
-      'react/jsx-dev-runtime',
-    ],
-  },
 };
